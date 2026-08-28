d46973060e532ca92312317488ef054bf0f7c9b4
OmidM5CS <omsua002@osloskolen.no>
2026-08-28 11:17:24 +0200

    Commit for "test-nettside"
    Dette er en commit for "test-nettside".

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-// NATT/DAG-MODUS
+// DAG/NATT-MODUS
 
-const toggleBtn = document.getElementById('toggleMode'); 
-toggleBtn.addEventListener('click', () => {
-  const isDark = document.documentElement.style.colorScheme === 'dark' || 
-                 (document.documentElement.style.colorScheme !== 'light' && window.matchMedia('(prefers-color-scheme: dark)').matches);
- 
-  document.documentElement.style.colorScheme = isDark ? 'light' : 'dark';
+const toggleBtn = document.getElementById("toggleMode");
+toggleBtn.addEventListener("click", () => {
+  const isDark =
+    document.documentElement.style.colorScheme === "dark" ||
+    (document.documentElement.style.colorScheme !== "light" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+  document.documentElement.style.colorScheme = isDark ? "light" : "dark";
 });
 
-// NATT/DAG-MODUS
+// DAG/NATT-MODUS
 
 // POENGTELLER
+
+let counterDisplayElem = document.querySelector(".counter-display");
+let counterMinusElem = document.querySelector(".counter-minus");
+let counterPlusElem = document.querySelector(".counter-plus");
+
+let count = 0;
+
+updateDisplay();
+
+counterPlusElem.addEventListener("click", () => {
+  count++;
+  updateDisplay();
+});
+
+counterMinusElem.addEventListener("click", () => {
+  count--;
+  updateDisplay();
+});
+
+function updateDisplay() {
+  counterDisplayElem.innerHTML = count;
+}
 
 // POENGTELLER
